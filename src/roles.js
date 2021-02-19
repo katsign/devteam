95f62926508d3e571ceb8bdc5d8c0db2fcb64658
Kat Buchanan <katrbuch@gmail.com>
2021-02-19 08:48:08 -0600

    setting up role generation functions

---
+++ src/roles.js
@@ -0,0 +1,17 @@
+function genManager(manager) {
+    return ``;
+}
+
+function genEngineer(engineer) {
+    return ``;
+}
+
+function genIntern(intern) {
+    return ``;
+}
+
+module.exports = {
+    genManager,
+    genEngineer,
+    genIntern
+}
