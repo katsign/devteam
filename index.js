0524db5e5b7e8b32f7fd95d095cb542ae9c03260
Kat Buchanan <katrbuch@gmail.com>
2021-02-19 08:35:59 -0600

    setting up inquirer/chalk trigger prompt

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const inquirer = require('inquirer');
-const Chalk = require('chalk');
+const chalk = require('chalk');
 const init = require('./src/prompts');
 
+const prompt = inquirer.createPromptModule();
+
+prompt([
+    {
+      type: 'list',
+      message: `${chalk.black.bgCyan}(
+        'Welcome. Should we start building your DevTeam?'
+      )}`,
+      choices: ['Start', 'Not Ready'],
+      name: "start",
+    },
+  ]).then((response) => {
+    switch (response.start) {
+      case 'Start':
+        init();
+        break;
+      case 'Not Ready':
+        return console.log(chalk.black.bgRed('Restart application and try again.'));
+    }
+  });
