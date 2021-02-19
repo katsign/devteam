aa82ff7d2b749e4c7cbe26691d0e3f1b46d925ef
Kat Buchanan <katrbuch@gmail.com>
2021-02-19 12:47:15 -0600

    cleaning

--- src/prompt.js
+++ src/prompt.js
@@ -1,10 +1,12 @@
 const Engineer = require('../lib/Engineer'),
   Intern = require('../lib/Intern'),
   Manager = require('../lib/Manager'),
-  { mHTML, eHTML, iHTML } = require('./employeeHTML'),
-  genHTML = require('./html'),
+  { mHTML, eHTML, iHTML } = require('./roles'),
+  generateHTML = require('./html'),
   css = require('./css'),
   chalk = require("chalk"),
   inquirer = require("inquirer"),
   prompt = inquirer.createPromptModule(),
   fs = require("fs");
+
+
