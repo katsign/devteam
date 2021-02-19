a4fc0aeb55d3af2aef7862b833ff96345a55c2c2
Kat Buchanan <katrbuch@gmail.com>
2021-02-19 10:14:41 -0600

    requiring new modules for prompt build

---
+++ src/prompt.js
@@ -0,0 +1,10 @@
+const Engineer = require('../lib/Engineer'),
+  Intern = require('../lib/Intern'),
+  Manager = require('../lib/Manager'),
+  { mHTML, eHTML, iHTML } = require('./employeeHTML'),
+  genHTML = require('./html'),
+  css = require('./css'),
+  chalk = require("chalk"),
+  inquirer = require("inquirer"),
+  prompt = inquirer.createPromptModule(),
+  fs = require("fs");
